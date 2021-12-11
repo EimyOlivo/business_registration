e82ff15e6271bb3b4ea00d7fdcbf9806b853fb18
koryroot <20197914@itla.edu.do>
2021-12-11 02:53:38 -0400

    create-read

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -10,10 +10,34 @@ export interface Item{
   longitude: number,
 }
 
+const ITEMS_KEY = 'my-items'
+
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  constructor(private storage: Storage) { }
+
+  //create by kory
+  addItem(item:Item) :Promise <any>{
+
+    return this.storage.get (ITEMS_KEY).then((items:[])=>{
+      if (items){
+        items.push(item);
+        return this.storage.set(ITEMS_KEY, items);
+
+      }
+      else{
+        return this.storage.set(ITEMS_KEY, [item]);
+      }
+
+    });
+
+  }
+  //read by kory
+  getItem(item:Item):Promise<Item[]>{
+
+    return this.storage.get(ITEMS_KEY);
+  }
 }
